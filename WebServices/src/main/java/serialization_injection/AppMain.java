1fd3f5f4cd42cfacce1c5881a7b28863a1ff763a
Andrei.Tokmakov <andtokm@yandex.ru>
2024-11-21 07:32:47 +0400

    21_11_2024_002

--- WebServices/src/main/java/serialization_injection/AppMain.java
+++ WebServices/src/main/java/serialization_injection/AppMain.java
@@ -3,6 +3,7 @@ package serialization_injection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class AppMain
 {
