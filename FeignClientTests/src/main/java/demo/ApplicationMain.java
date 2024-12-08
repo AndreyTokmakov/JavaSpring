aa3b58baa5b436332dff9da2888e9d13a7b27292
AndreyTokmakov <andtokm@yandex.ru>
2024-12-08 21:03:20 +0400

    08_12_2024__008

--- FeignClientTests/src/main/java/demo/ApplicationMain.java
+++ FeignClientTests/src/main/java/demo/ApplicationMain.java
@@ -1,16 +1,19 @@
 package demo;
 
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 
 @EnableConfigurationProperties
 // @EnableDiscoveryClient
-// @EnableFeignClients(basePackages = "demo")
-@EnableFeignClients
+@EnableFeignClients(clients=EternalServiceClient.class)
+// @EnableFeignClients
 @SpringBootApplication(scanBasePackages = {"demo"})
+// @ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class ApplicationMain
 {
 	public static void main(String[] args) {
--- FeignClientTests/src/main/java/demo/EternalServiceClient.java
+++ FeignClientTests/src/main/java/demo/EternalServiceClient.java
@@ -4,9 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "giveYourServiceName",
-             url = "provideYourUrlHere",
-             path = "provideYourContextPathHere")
+
+@FeignClient(name = "test-service",
+             url = "https://some-test-server.com/api")
 public interface EternalServiceClient
 {
     @GetMapping(value = "/posts/{postId}", produces = "application/json")
