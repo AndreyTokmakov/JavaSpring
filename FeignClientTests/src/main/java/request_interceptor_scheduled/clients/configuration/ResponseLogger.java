e8ea2f711054daa1df278d8f38a07e55acac3781
Andrei.Tokmakov <andtokm@yandex.ru>
2025-03-20 18:23:28 +0400

    20_03_2025_05

--- FeignClientTests/src/main/java/request_interceptor_scheduled/clients/configuration/ResponseLogger.java
+++ FeignClientTests/src/main/java/request_interceptor_scheduled/clients/configuration/ResponseLogger.java
@@ -4,22 +4,16 @@ package request_interceptor_scheduled.clients.configuration;
 import feign.*;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
-
 import java.io.IOException;
-import java.io.Reader;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 
 
 public class ResponseLogger
 {
     @Log4j2
-    private final static class SimpleLogger extends Logger
+    private final static class DeribitResponseLogger extends Logger
     {
         @Override
-        protected void log(String configKey, String format, Object... args)
-        {
-            //System.err.printf(configKey + " - " + format + "%n", args);
+        protected void log(String configKey, String format, Object... args)  {
         }
 
         @Override
@@ -28,27 +22,20 @@ public class ResponseLogger
                                                   Response response,
                                                   long elapsedTime) throws IOException
         {
-            Response.Body body = response.body();
+            final Response.Body body = response.body();
             if (null != body)
             {
                 byte[] bodyData = Util.toByteArray(body.asInputStream());
                 response = response.toBuilder().body(bodyData).build();
-                Request request = response.request();
-
                 log.info(new String(bodyData));
-                log.info("{} {}", request.httpMethod().name(), request.url());
+                log.info("{} {}", response.request().httpMethod().name(), response.request().url());
             }
             return super.logAndRebufferResponse(configKey, logLevel, response, elapsedTime);
         }
     }
 
     @Bean
-    Logger.Level getLoggerLevel() {
-        return Logger.Level.FULL;
-    }
-
-    @Bean
     public Logger logger() {
-        return new SimpleLogger();
+        return new DeribitResponseLogger();
     }
 }
