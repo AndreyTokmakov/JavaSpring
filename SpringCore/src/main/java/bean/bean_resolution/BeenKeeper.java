b3e74124e27b3648f1bd5ce856f8919a53e358a4
AndreyTokmakov <andtokm@yandex.ru>
2025-04-18 18:27:34 +0400

    18_04_2025__003

--- SpringCore/src/main/java/bean/bean_resolution/BeenKeeper.java
+++ SpringCore/src/main/java/bean/bean_resolution/BeenKeeper.java
@@ -2,6 +2,7 @@ package bean.bean_resolution;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Log4j2
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Component;
 public class BeenKeeper
 {
     @Autowired
+    //@Qualifier("makeProductPrimary")
+    @Qualifier("makeProductSecondary")
     private Product product;
 
     public BeenKeeper()
